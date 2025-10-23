75862273009fc8b25b5f8c5fad57c114325bc32d
manishsamota009 <29772414@students.lincoln.ac.uk>
2025-10-23 14:54:01 +0000

    Factorial problem and bank account process solved

--- bank_account.hpp
+++ bank_account.hpp
@@ -1,5 +1,8 @@
+
 #include <iostream>
 #include <string>
+
+using namespace std;
 
 class BankAccount {
 private:
@@ -11,25 +14,55 @@ public:
     // Constructor
     BankAccount(){
         // TODO: Implement the constructor
+        cout << "Constructor" << endl;
+        accountNumber = "";
+        accountHolderName = "";
+        balance = 0.0;
     }
 
-    BankAccount(const std::string& accNumber, const std::string& accHolder, double initialBalance){
+   BankAccount(const std::string& accNumber, const std::string& accHolder, double initialBalance){
         // TODO: Implement the constructor
+        accountNumber = accNumber;
+        accountHolderName = accHolder;
+        if (initialBalance < 0) {
+            cout << "Initial Balance cannot be negative. Balance set to 0." << endl;
+            balance = 0.0;
+        }
+        else {
+            balance= initialBalance;
+        }
+
     }
 
     // Deposit method
     void Deposit(double amount) {
-        // TODO: Implement the Deposit method
+        if(amount<0){
+            cout <<"Negative amont is not premitted";
+        }
+        else{
+        balance += amount;
+        cout << "Current Balance: " << balance << endl;
+        }
+        
     }
 
     // Withdraw method
     void Withdraw(double amount) {
-        // TODO: Implement the Deposit method
+       if (amount <= balance) {
+            balance -= amount;
+        cout << "Current Balance: " << balance << endl;
+        }
+        else {
+            cout << "Insufficnet Funds." << endl;
+        }
     }
 
     // Display account information
     void DisplayAccountInfo() {
         // TODO: Implement the DisplayAccountInfo method
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Account Holder Name: " << accountHolderName << endl;
+        cout << "Balance: £" << balance << endl;
     }
 
     double GetBalance() const {
@@ -37,5 +70,3 @@ public:
         return balance;
     }
 };
-
-
